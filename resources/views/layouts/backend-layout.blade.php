4a8f02ba74318abcd8a6e26ea127294871dded7f
edu <89234306+edu455@users.noreply.github.com>
2024-03-27 06:56:24 -0500

    Category and logout routes added

--- resources/views/layouts/backend-layout.blade.php
+++ resources/views/layouts/backend-layout.blade.php
@@ -69,7 +69,7 @@
                 </a>
             </li>
             <li>
-                <a href="#"
+                <a href="{{route('backend.category.index')}}"
                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                     <i class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white fa-solid fa-table-list"></i>
                     <span class="flex-1 ms-3 whitespace-nowrap">Categories</span>
@@ -77,11 +77,12 @@
             </li>
 
             <li>
-                <a href="#"
-                   class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <i class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white fa-solid fa-right-from-bracket"></i>
-                    <span class="flex-1 ms-3 whitespace-nowrap">Log out</span>
-                </a>
+                <form action="{{route('logout')}}" method="post" class="w-full">
+                    <button  class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <i class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white fa-solid fa-right-from-bracket"></i>
+                        <span class="flex-1 ms-3 whitespace-nowrap">Log out</span>
+                    </button>
+                </form>
             </li>
         </ul>
     </div>
