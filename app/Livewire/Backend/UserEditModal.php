d7ab3f125d2a7b71085be7fa9893efd4a9e2abcb
edu <89234306+edu455@users.noreply.github.com>
2024-03-28 11:45:23 -0500

    Backend user edit modal

---
+++ app/Livewire/Backend/UserEditModal.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Livewire\Backend;
+
+use App\Models\Department;
+use App\Models\User;
+use Livewire\Component;
+use LivewireUI\Modal\ModalComponent;
+
+class UserEditModal extends ModalComponent
+{
+    public User $user;
+    public string $name = '';
+    public string $email = '';
+    public string $password = '';
+    public string $password_confirmation = '';
+    public int $type = 1;
+    public string $phone = '';
+    public string $address = '';
+//    public string $image = '';
+    public ?string $department_id = null;
+
+    public bool $isDisabled = false;
+
+    public function mount(User $user)
+    {
+        $this->user = $user;
+        $this->name = $user->name;
+        $this->email = $user->email;
+        $this->type = $user->type;
+        $this->phone = $user->phone;
+        $this->address = $user->address;
+        $this->department_id = $user->department_id;
+    }
+
+    public function render()
+    {
+        return view('livewire.backend.user-edit-modal', ['departments' => Department::all()]);
+    }
+
+    public function departmentInputStatus()
+    {
+        if ($this->type !== 2) {
+            $this->isDisabled = true;
+        } else {
+            $this->isDisabled = false;
+        }
+    }
+
+    public function update_user()
+    {
+        $this->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email,' . $this->user->id,
+            'type' => 'required|integer',
+            'phone' => 'required|string',
+            'address' => 'required|string',
+            'department_id' => 'nullable|integer',
+            'password' => 'nullable|string|confirmed',
+        ]);
+
+        $this->user->update([
+            'name' => $this->name,
+            'email' => $this->email,
+            'type' => $this->type,
+            'phone' => $this->phone,
+            'image' => 'default.jpg',
+            'address' => $this->address,
+            'department_id' => $this->department_id,
+            'password' => $this->password ? $this->password : $this->user->password,
+        ]);
+
+        toastr()->success('User updated successfully');
+        $this->dispatch('pg:eventRefresh-default');
+        $this->closeModal();
+    }
+}
--- app/Livewire/BackendUsersTable.php
+++ app/Livewire/BackendUsersTable.php
@@ -66,7 +66,7 @@ final class BackendUsersTable extends PowerGridComponent
             })
             ->add('phone')
             ->add('address')
-            ->add('department',function ($row) {
+            ->add('department', function ($row) {
                 return $row->department?->name;
             });
     }
@@ -123,7 +123,8 @@ final class BackendUsersTable extends PowerGridComponent
                 ->render(function (User $user) {
                     return \Blade::render(<<<HTML
                     <div class="flex items-center justify-center">
-                        <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+                        <button type="button" onclick="Livewire.dispatch('openModal', { component: 'backend.user-edit-modal', arguments:{ user: $user->id  }})"
+                         class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
                             Edit
                         </button>
                         
