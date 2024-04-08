f87694b383e97bb90f495d9e253e8f98ac82fd40
edu <89234306+edu455@users.noreply.github.com>
2024-04-08 07:11:39 -0500

    Backend attached files modal fix

--- app/Livewire/Backend/TicketAttachedFilesModal.php
+++ app/Livewire/Backend/TicketAttachedFilesModal.php
@@ -4,11 +4,15 @@ namespace App\Livewire\Backend;
 
 use App\Models\Ticket;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 use LivewireUI\Modal\ModalComponent;
 
 class TicketAttachedFilesModal extends ModalComponent
 {
+    use WithFileUploads;
+
     public Ticket $ticket;
+    public $files = [];
     public function render()
     {
         return view('livewire.backend.ticket-attached-files-modal');
@@ -16,6 +20,23 @@ class TicketAttachedFilesModal extends ModalComponent
 
     public static function modalMaxWidth(): string
     {
-        return 'md';
+        return '2xl';
+    }
+
+    public function save_files()
+    {
+        $this->validate([
+            'files.*' => 'required|file|mimes:pdf,jpeg,png,jpg|max:1024',
+        ]);
+
+        foreach ($this->files as $file) {
+            $new_stored_image = basename($file->store('public/uploads'));
+            $this->ticket->files()->create([
+                'name' => $new_stored_image,
+            ]);
+        }
+
+        toastr()->success('Files uploaded successfully!');
+        $this->closeModal();
     }
 }
--- resources/views/livewire/backend/ticket-attached-files-modal.blade.php
+++ resources/views/livewire/backend/ticket-attached-files-modal.blade.php
@@ -1,13 +1,28 @@
+@php
+    function formatBytes($bytes, $precision = 2) {
+        $units = array('B', 'KB', 'MB', 'GB', 'TB');
+
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+
+        $bytes /= pow(1024, $pow);
+
+        return round($bytes, $precision) . ' ' . $units[$pow];
+    }
+ @endphp
 <div class="w-full flex flex-col">
     <div class="flex justify-center border-b">
         <h3 class="py-4 text-xl font-medium">Ticket attached files</h3>
     </div>
-   <div class="px-4 py-12">
-       <ul>
-           <li>Document_1.pdf</li>
-           <li>Document_2.png</li>
-           <li>Document_3.png</li>
-           <li>Document_4.png</li>
-       </ul>
-   </div>
+    <div class="px-4 py-12">
+        <ul class="mb-12 text-lg">
+            @forelse($ticket->files as $file)
+                <li> {{$file->name}} <span class="text-gray-500 text-sm">{{formatBytes(Storage::size("public/uploads/$file->name"))}}</span>  <a href="{{route('download.file',$file)}}" class="ms-2"><i
+                                class="fa-solid fa-file-arrow-down"></i></a></li>
+            @empty
+            @endforelse
+        </ul>
+    </div>
+
 </div>
