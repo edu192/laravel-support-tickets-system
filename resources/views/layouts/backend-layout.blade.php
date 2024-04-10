173ea648baa6a582c6c70165aef667e0365e6ee3
edu <89234306+edu455@users.noreply.github.com>
2024-04-10 09:19:54 -0500

    Sidebar layout active links

--- resources/views/layouts/backend-layout.blade.php
+++ resources/views/layouts/backend-layout.blade.php
@@ -106,24 +106,27 @@
                         aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
                     <i class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white   fa-solid fa-ticket"></i>
                     <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Tickets</span>
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    <svg class="w-3 h-3"
+                         aria-hidden="{{request()->routeIs(['backend.ticket.index', 'backend.ticket.unassigned', 'backend.ticket.assigned'])}}"
+                         xmlns="http://www.w3.org/2000/svg" fill="none"
                          viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                               d="m1 1 4 4 4-4"/>
                     </svg>
                 </button>
-                <ul id="dropdown-example" class="hidden py-2 space-y-2">
+                <ul id="dropdown-example"
+                    class="{{request()->routeIs(['backend.ticket.index', 'backend.ticket.unassigned', 'backend.ticket.assigned'])?'':'hidden'}} py-2 space-y-2">
                     <li>
                         <a href="{{route('backend.ticket.assigned')}}"
-                           class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Assigned</a>
+                           class="{{request()->routeIs( 'backend.ticket.assigned')?'bg-gray-100':''}} flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Assigned</a>
                     </li>
                     <li>
                         <a href="{{route('backend.ticket.unassigned')}}"
-                           class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Unassigned</a>
+                           class="{{request()->routeIs( 'backend.ticket.unassigned')?'bg-gray-100':''}} flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Unassigned</a>
                     </li>
                     <li>
                         <a href="{{route('backend.ticket.index')}}"
-                           class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">All</a>
+                           class="{{request()->routeIs( 'backend.ticket.index')?'bg-gray-100':''}} flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">All</a>
                     </li>
                 </ul>
             </li>
