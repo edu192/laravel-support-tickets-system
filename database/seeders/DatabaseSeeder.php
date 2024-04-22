a68990cf87efc8e72bfa4893e300f44e316793c7
edu <89234306+edu455@users.noreply.github.com>
2024-04-22 18:07:58 -0500

    Call all seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,7 +10,8 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
+    : void
     {
         // \App\Models\User::factory(10)->create();
 
@@ -18,5 +19,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call(DepartmentSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(TicketSeeder::class);
+        $this->call(CommentSeeder::class);
     }
 }
